87b67529b2e1584cb433b94c61487bc0a1ca472d
Niklas H <156240330+mymh13@users.noreply.github.com>
2025-05-06 14:50:15 +0200

    Bugfix: Updating DB Context

--- api/Data/SaunaBookingDbContext.cs
+++ api/Data/SaunaBookingDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SaunaBooking.Api.Models;
 
 namespace SaunaBooking.Api.Data
@@ -11,7 +12,19 @@ namespace SaunaBooking.Api.Data
         }
 
         public DbSet<User> Users { get; set; }
-
         public DbSet<Booking> Bookings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Prevent DateTimeKind=Utc issues on Booking.Date
+            var dateConverter = new ValueConverter<DateTime, DateTime>(
+                v => DateTime.SpecifyKind(v, DateTimeKind.Unspecified), // Save as unspecified
+                v => DateTime.SpecifyKind(v, DateTimeKind.Unspecified)  // Load as unspecified
+            );
+
+            modelBuilder.Entity<Booking>()
+                .Property(b => b.Date)
+                .HasConversion(dateConverter);
+        }
     }
 }
