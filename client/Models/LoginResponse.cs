d0626139e37b4ba33dea04d4fdb7942ae0901e11
Niklas H <156240330+mymh13@users.noreply.github.com>
2025-05-07 13:49:19 +0200

    Bugfix: missed a reference

--- client/Models/LoginResponse.cs
+++ client/Models/LoginResponse.cs
@@ -5,5 +5,6 @@ namespace SaunaBooking.Client.Models
         public bool Success { get; set; }
         public string Message { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
     }
 }
